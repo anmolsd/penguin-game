a70bc50d66fd39f5ba92d3ab7d7b3832dfcdd6e4
anmolsd <anmolsinghdhaliwal03@gmail.com>
2022-04-27 06:52:13 -0400

    Updates

---
+++ precache-manifest.54ab2820d64690fa6ab9d44863409cff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "709599348583ddbf18a3",
+    "url": "/penguin-game/static/css/main.783ca8df.chunk.css"
+  },
+  {
+    "revision": "709599348583ddbf18a3",
+    "url": "/penguin-game/static/js/main.70959934.chunk.js"
+  },
+  {
+    "revision": "77b7e22a431588bbce0b",
+    "url": "/penguin-game/static/js/1.77b7e22a.chunk.js"
+  },
+  {
+    "revision": "68bc55220a86872da5fe",
+    "url": "/penguin-game/static/js/runtime~main.68bc5522.js"
+  },
+  {
+    "revision": "28e1b3eceec905b2755f25742f0844bf",
+    "url": "/penguin-game/static/media/never_enough_edit.28e1b3ec.wav"
+  },
+  {
+    "revision": "ce0cf28e4d1e13d1a9ec64fa61a9f342",
+    "url": "/penguin-game/static/media/131711__oldedgar__aaron-helm.ce0cf28e.wav"
+  },
+  {
+    "revision": "63ac63f90e8bdae269c779bdd527c526",
+    "url": "/penguin-game/static/media/pause.63ac63f9.png"
+  },
+  {
+    "revision": "2cbb330297faf3110da79392cc477f0d",
+    "url": "/penguin-game/static/media/play_icon.2cbb3302.png"
+  },
+  {
+    "revision": "048832a90f81b898addb4d8f188e4b51",
+    "url": "/penguin-game/static/media/skipper.048832a9.png"
+  },
+  {
+    "revision": "9c5ea608126fbbbf305f519c1b27abe4",
+    "url": "/penguin-game/static/media/up_arrow.9c5ea608.png"
+  },
+  {
+    "revision": "8ee92d2226dfaaa6ccdf579d9a55c91c",
+    "url": "/penguin-game/index.html"
+  }
+];
